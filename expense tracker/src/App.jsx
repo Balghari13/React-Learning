98827f4f5184594b68e90ecc7390569ebe96b9ec
Balghari <najmbalghari13@gmail.com>
2025-11-21 11:44:00 +0500

    components
    make components of expense tracker

---
+++ expense tracker/src/App.jsx
@@ -0,0 +1,26 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import { ExpenseForm } from './components/ExpenseForm'
+import { TableExpense } from './components/ExpenseTable'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <main>
+     <h1>Track Your Expense</h1>
+    <div className="expense-tracker">
+        <ExpenseForm/>
+        <TableExpense/>
+        <div className="context-menu">
+            <div>Edit</div>
+            <div>Delete</div>
+        </div>
+      </div>
+    </main>
+  )
+}
+
+export default App
