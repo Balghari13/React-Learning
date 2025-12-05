0d4d740e3cc31d9884ac9770e2c43431d8315a10
Balghari <najmbalghari13@gmail.com>
2025-12-05 11:02:27 +0500

    create useLocalStoragehook

--- expense tracker/src/App.jsx
+++ expense tracker/src/App.jsx
@@ -4,6 +4,7 @@ import './App.css'
 import { ExpenseForm } from './components/ExpenseForm'
 import { TableExpense } from './components/ExpenseTable'
 import expenseData from './components/expenseData'
+import { useLocalStorage } from './hooks/useLocalStorage'
 
 function App() {
   const [expenses, setExpenses] = useState(expenseData)
@@ -14,11 +15,12 @@ function App() {
   });
 
   const[isEditing, setIsEditing] = useState('')
-
+const[localData, setLocalData] = useLocalStorage('num',[1,2,3])
+  console.log(localData);
   
   return (
     <main>
-     <h1>Track Your Expense</h1>
+     <h1 onClick={()=>setLocalData((pre)=>[...pre,4,5,6])}>Track Your Expense {localData}</h1>
     <div className="expense-tracker">
         <ExpenseForm setExpenses={setExpenses} expense={expense} setExpense={setExpense} isEditing={isEditing} setIsEditing={setIsEditing}/>
         <TableExpense expenses = {expenses} setExpenses={setExpenses} setExpense={setExpense} setIsEditing={setIsEditing}/>
